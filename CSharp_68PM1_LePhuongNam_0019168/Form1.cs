253ec934d9718ad4997531fbb09907ab0237669d
nammoc-2005 <namty2005@gmail.com>
2026-05-13 23:06:56 +0700

    Add project files.

---
+++ CSharp_68PM1_LePhuongNam_0019168/Form1.cs
@@ -0,0 +1,10 @@
+namespace CSharp_68PM1_LePhuongNam_0019168
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
