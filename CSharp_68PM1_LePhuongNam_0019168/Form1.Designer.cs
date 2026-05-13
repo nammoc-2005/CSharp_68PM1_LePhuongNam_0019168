e398ddeecf3d364dd09aabfbef1973c8b5fb1a52
nammoc-2005 <namty2005@gmail.com>
2026-05-13 23:10:18 +0700

    Chức năng đăng nhập

--- CSharp_68PM1_LePhuongNam_0019168/Form1.Designer.cs
+++ CSharp_68PM1_LePhuongNam_0019168/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblTitle = new Label();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Location = new Point(355, 31);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(95, 20);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "ĐĂNG NHẬP";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(266, 71);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(110, 20);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Tên đăng nhập:";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(266, 121);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(73, 20);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Mật khẩu:";
+            lblPassword.Click += lblPassword_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(404, 71);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(404, 118);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(355, 197);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(lblTitle);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
     }
 }
--- CSharp_68PM1_LePhuongNam_0019168/Form1.cs
+++ CSharp_68PM1_LePhuongNam_0019168/Form1.cs
@@ -6,5 +6,31 @@ namespace CSharp_68PM1_LePhuongNam_0019168
         {
             InitializeComponent();
         }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+            string emailSinhVien = "namty2005@gmail.com";
+            string mssv = "0019168";
+
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (username == emailSinhVien && password == mssv)
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại! Vui lòng kiểm tra lại.", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
